5e558b61f6fb946039ea7d1f0948bd59f8823843
sebasss21 <sr017235@gmail.com>
2024-10-31 18:44:12 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1730320855,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/juego/bruja-sheet0.png",
+		"images/juego/pincelinferior-sheet0.png",
+		"images/juego/pincelsuperior-sheet0.png",
+		"images/juego/nube-sheet0.png",
+		"images/juego/fondo.png",
+		"images/juego/sprite-sheet0.png",
+		"images/juego/moneda-sheet0.png",
+		"images/juego/limiteizquierdo-sheet0.png",
+		"media/juego/brass-fail-1-c-185076.ogg",
+		"media/juego/game-music-loop-7-145285.ogg",
+		"media/juego/scary-witch-laugh-sound-effect-_-risa-de-bruja-efecto-de-sonido_m4a_128k_.ogg",
+		"media/juego/sonido-golpe_m4a_128k_.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
